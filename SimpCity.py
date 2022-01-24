990ee048d4709596ed953d83416837cdec0447df
jamz903 <64241899+jamz903@users.noreply.github.com>
2022-01-24 15:38:30 +0800

    Update SimpCity.py

--- SimpCity.py
+++ SimpCity.py
@@ -1,5 +1,3 @@
 from start_menu import *
 
-start_menu(-1, True, ["2","1","0"], buildings=["HWY", "FAC", "SHP", "SHP"], locations=["D4"], bldgChoicesList=["BCH,SHP,HWY,FAC,HSE"], board_size_list=["4,4"], lb_names=["","nameWithPunc!@#$%","abcdabcdabcdabcdabcda","abcdabcdabcdabcdabcdab","abcdabcdabcdabcdabcd"])
-
-# start_menu(-1)
+start_menu(-1)
